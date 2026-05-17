96f87e0b854a62fd11fc745a8cfa7869380513b1
Sathavaram Lahare <142076496+sathavaramlahare@users.noreply.github.com>
2026-05-17 20:32:50 +0530

    Create device.h

---
+++ Header_Files/device.h
@@ -0,0 +1,12 @@
+// DEVICE TIME VARIABLES
+void controldevices(void);
+extern int on_hour1,on_min1,on_sec1;
+extern int off_hour1,off_min1,off_sec1;
+
+extern int on_hour2,on_min2,on_sec2;
+extern int off_hour2,off_min2,off_sec2;
+
+extern int Device_1_ON_Time;
+extern int Device_1_OFF_Time;
+extern int Device_2_ON_Time;
+extern int Device_2_OFF_Time;
