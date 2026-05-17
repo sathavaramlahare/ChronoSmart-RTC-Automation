3d4c05140666eb12b6806f650ccbf427809a6209
Sathavaram Lahare <142076496+sathavaramlahare@users.noreply.github.com>
2026-05-17 20:13:07 +0530

    Update adc.c

--- source_code/adc.c
+++ source_code/adc.c
@@ -1,32 +1,6 @@
 #include<lpc21xx.h>
-#include"Defines.h"
-#define FOSC 12000000
-#define CCLK (5*FOSC)
-#define PCLK (CCLK/4)
-//ADC clk frequency is lessthan 4.5MHZ
-#define ADCCLK 3000000 
-// Clock divider value for ADC
-#define CLK_DIV ((PCLK/ADCCLK)-1)
-// Power Down bit position in ADCR register
-#define PDN 1<<21
-// ADC clock divider value shifted to correct position
-#define CLKDIV_VALUE CLK_DIV<<8
-// Start conversion bit
-#define START_CONV 1<<24
-// ADC conversion completion bit
-#define DONE_BIT 31
-// ADC result bit position
-#define Result 6
-// ADC channel selection values
-#define CH0 0x01
-#define CH1 0x02
-#define CH2 0x04
-#define CH3 0x08
-// PINSEL1 configuration values for ADC pins
-#define AIN0 0x00400000
-#define AIN1 0x01000000
-#define AIN2 0x04000000
-#define AIN3 0x10000000
+#include "adc.h"
+#include "delay.h"
 
 //Initializes ADC peripheral and configures ADC pin
 void INIT_ADC(void)
