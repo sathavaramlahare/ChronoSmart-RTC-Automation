e991745bdd3783ad0bae409c075c50e0b267beef
Sathavaram Lahare <142076496+sathavaramlahare@users.noreply.github.com>
2026-05-17 20:22:59 +0530

    Create lcd.h

---
+++ Header_Files/lcd.h
@@ -0,0 +1,10 @@
+//LCD function declarations
+void write_lcd(int data);
+void cmd_lcd(int data);
+void char_lcd(int data);
+void LCD_INIT(void);
+void str_lcd(char *str);
+void int_LCD(int n);
+void neg_lcd(int n);
+void float_lcd(float f,int nDP);
+void delay_ms(unsigned int dly);
